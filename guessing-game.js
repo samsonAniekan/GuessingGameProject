13a453db6a1dabb3495df434ff0f9db47c0684e0
Aniekan-Samson <aniekansamsonubong@gmail.com>
2024-04-22 23:53:08 +0100

    added two new functionalities to aid functionality

--- guessing-game.js
+++ guessing-game.js
@@ -1,4 +1,12 @@
-const secretNumber = 4;
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+// the readline method displays ansnwers on the standard output, it takes the askGuess function as callback
+//rl.question("Enter a guess: ", askGuess)
+
 
 function checkGuess(number) {
     if (number > secretNumber) {
@@ -16,27 +24,49 @@ function checkGuess(number) {
         return true;
     }
 }
+ let numberOFAttempts = 5;
 
-
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-// the readline method displays ansnwers on the standard output, it takes the askGuess function as callback
-rl.question("Enter a guess: ", askGuess)
+let secretNumber = randomInRange(0, 100)
 function askGuess(firstAnswer) {
-    if(checkGuess(Number(firstAnswer)) === true) {
+    // if (typeof firstAnswer !== 'number') {
+    //     return false;
+    // }
+    numberOFAttempts--
+    if(checkGuess(Number(firstAnswer))) {
         console.log("you win");
         rl.close()
     }
-       else if (Number(firstAnswer) > secretNumber) {
-         console.log("try again");
-         rl.question("Enter another guess: ", askGuess);
-     }
 
-    else {
-        console.log("you lose!");
-        rl.close();
+    if (numberOFAttempts === 0) {
+        console.log("you LOSE");
+         return rl.close();
     }
+
+    rl.question("Enter another guess ", askGuess);
+
 }
+
+function randomInRange(min, max) {
+    const minNum = Math.ceil(min);
+    const maxNum = Math.floor(max);
+    return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+}
+
+/** when askRaneg is called it runs the block of code and the calls the ask guess function for next steps **/
+function askRange() {
+    rl.question("Enter a Min Number: ", (min) => {
+        rl.question("Enter a Max Number: ", (max) => {
+            console.log(`I am thinking of a number between ${min} and ${max}...`);
+            secretNumber = randomInRange(Number(min), Number(max));
+            rl.question("enter a guess ", askGuess);
+        })
+    })
+}
+/**the whole game starts here at the askLimit function and the calls ask Range function */
+function askLimit() {
+    rl.question("Please enter Game Attempts: ", (limit) => {
+        numberOFAttempts = Number(limit);
+        askRange();
+    })
+}
+ askLimit();
